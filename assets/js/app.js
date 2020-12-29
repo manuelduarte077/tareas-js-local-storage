ee8607241f167eb9990dd04f249f3e0620384125
manuelduarte077 <carlosd99m@gmail.com>
2020-12-29 14:59:26 -0600

    Agregando a localStorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,88 @@
 // Variables
-const listaTareas = document.getElementById('lista-tarea')
-
-
+const listaTareas = document.getElementById( "lista-tarea" );
 
 // Event Listener
 
 eventListeners();
 
-
-function eventListeners (){
+function eventListeners() {
   // Cuando se envia el formulario
 
-  document.querySelector('#formulario').addEventListener('submit', 
-  agregarTarea);
+  document
+    .querySelector("#formulario")
+    .addEventListener("submit", agregarTarea);
 
-  listaTareas.addEventListener('click', borrarTarea);
+  listaTareas.addEventListener("click", borrarTarea);
+
+  // Contenido cargado
+  // document.addEventListener("DOMContentLoaded", localStorageListo);
+}
+
+// Funciones
+
+// Añadir tarea del formulario
+function agregarTarea(e) {
+  e.preventDefault();
+  // Leer el valor del textarea
+  const tarea = document.getElementById( "tarea" ).value;
+
+  // crear boton de eliminar
+  const botonBorrar = document.createElement( "a" );
+  botonBorrar.classList = "borrar-tarea";
+  botonBorrar.innerText = "X";
+
+  // Crear elemento y añadirle el contenido a la lista
+  const li = document.createElement( "li" );
+  li.innerText = tarea;
+
+  // añade el botón de borrar la tarea
+  li.appendChild( botonBorrar );
+
+  // añade la tarea a la lista
+  listaTareas.appendChild( li );
+
+
+  // Añadir a Local Storage
+  agregarTareaLocalStorage( tarea );
+
+}
+
+// Borrar tarea
+function borrarTarea( e ) {
+  e.preventDefault();
+
+  if ( e.target.className === "borrar-tarea" ) {
+    console.log( e.target.parentElement.remove() );
+    alert( "Tarea Eliminada" );
+  }
+}
+
+
+// Agrega tarea a local storage
+
+function agregarTareaLocalStorage(tarea) {
+  let tareas;
+  tareas = obtenerTareaLocalStorage();
+
+  // Añadir la nueva tarea
+  tareas.push(tarea);
+
+  // Convertir de string a arreglo para local storage
+  localStorage.setItem( 'tareas', JSON.stringify( tareas ) );
 
 }
 
 
-// Funciones
+function obtenerTareaLocalStorage () {
+  let tareas;
 
-
-
-// Añadir tarea del formulario
- function agregarTarea(e) {
-   e.preventDefault();
-    // Leer el valor del textarea
-    const tarea = document.getElementById('tarea').value;
-
-    // crear boton de eliminar
-    const botonBorrar = document.createElement('a');
-    botonBorrar.classList = 'borrar-tarea';
-    botonBorrar.innerText = 'X';
-
-    // Crear elemento y añadirle el contenido a la lista
-    const li = document.createElement('li');
-    li.innerText = tarea;
-
-    // añade el botón de borrar la tarea
-    li.appendChild(botonBorrar);
-
-    // añade la tarea a la lista
-    listaTareas.appendChild(li);
-
-
- }
-
-
-// Borrar tarea
- function borrarTarea (e) {
-   e.preventDefault();
-   
-  if(e.target.className === 'borrar-tarea') {
-    console.log(e.target.parentElement.remove());
-    alert('Tarea Eliminada');
+  // Revisamos los valores del local storage
+  if( localStorage.getItem( 'tareas' ) === null ) {
+    tareas = [];
+  } else {
+    tareas = JSON.parse( localStorage.getItem( 'tareas' ) );
   }
 
- }
+  return tareas;
+
+}
